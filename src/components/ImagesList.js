24ab2a398e49d7a1ae61681d197cc45d3ac993cb
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-06-01 16:25:59 +0300

    added loading effect

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -7,17 +7,20 @@ const ImagesList = ( ) => {
 
     const [images, setImages] = useState([]);
     const [isLoading, setLoading] = useState(true);
+    const [isMorePages, setIsMorePages] = useState(false);
     const [page, setPage] = useState(1);
 
     const getMorePages = () => {
         instance.get(`/list`, {params: { page: `${page}`, limit: 10 }}).then((data) => {
             setImages([...images, ...data]);
             setLoading(false);
+            setIsMorePages(false);
         });
 
     }
 
     const handleClick = () => {
+        setIsMorePages(true);
         setPage((page) => page + 1);
     };
 
@@ -34,7 +37,7 @@ const ImagesList = ( ) => {
                  <li className="imageslist__item" key={id}> <img className="imageslist__image" src={download_url}/></li>
             ))}
         </List>
-        <Button color="primary" size="large" onClick={handleClick}>Show More</Button>
+        {isMorePages ? <p>Loading...</p> : <Button color="primary" size="large" onClick={handleClick}>Show More</Button>}
         </>
     );
 
